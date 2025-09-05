04eb62273eae4483ebc410e8a05c0ce7569ca87d
Sumit6728 <monikaraj0954@gmail.com>
2025-09-05 06:03:45 +0530

    Update and rename nach b.js to 00nach b.js

--- Priyansh/commands/00nach b.js
+++ Priyansh/commands/00nach b.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
      react.includes("nch") ||
 react.includes("nachh")) {
     var msg = {
-        body: `MAI TO NACHUNGI🙈😒😒👈`,attachment: fs.createReadStream(__dirname + `/noprefix/nach.mp4`)
+        body: `BASANTI NE NACHNA CHOD DIYA HAI AB🙈😒😒👈`,attachment: fs.createReadStream(__dirname + `/noprefix/nach.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("💃", event.messageID, (err) => {}, true)
